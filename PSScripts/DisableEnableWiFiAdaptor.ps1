b62d2832a4b92a82d32f73c535aa5e9e4921c415
Stephen Willson <stephendwillson@live.com>
2014-02-09 14:33:38 -0500

    Extra check in WiFi [dis/en]able script
    -Added a check in DisableEnableWiFiAdaptor.ps1 for WiFi adaptor not
    existing.

--- PSScripts/DisableEnableWiFiAdaptor.ps1
+++ PSScripts/DisableEnableWiFiAdaptor.ps1
@@ -1,10 +1,16 @@
-﻿#Need checks for no adaptor installed
-#Usage: Write-Error "Sample error text"
+﻿#Usage: Write-Error "Sample error text"
 
 
 
 $adaptorName = "*6250 AGN*"
 
 $adaptor = Get-WmiObject -Class Win32_NetworkAdapter | Where-Object {$_.Name -like $adaptorName}
+
+#If for some reason the adaptor can't be found, write an error and break from script
+If(!$adaptor) {
+    Write-Error "Network adaptor matching the pattern " + $adaptorName + " not found."
+    Break
+}
+
 $adaptor.Disable()
 $adaptor.Enable()
