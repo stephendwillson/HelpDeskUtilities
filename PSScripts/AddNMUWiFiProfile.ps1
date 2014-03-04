144636daf3a6569e8a8712d14bc184f6d3f1696b
Stephen Willson <stephendwillson@live.com>
2014-03-03 16:36:33 -0800

    Updated goals, delete NMU profile before adding
    -Updated goals to reflect latest tasks completed.
    -Updated AddNMUWiFiProfile.ps1 by request of Mark Bailey to delete WiFi
    profile named "NMU" at the start of the script rather than trying to add
    it no matter what and failing silently if profile exists.

--- PSScripts/AddNMUWiFiProfile.ps1
+++ PSScripts/AddNMUWiFiProfile.ps1
@@ -3,6 +3,8 @@
 # This script is poorly written, but after so much testing/tweaking, I can't make it work better.
 # Grabs the location HelpDesk Utilities was launched from, appends \PSScripts onto it, then appends \Wi-Fi-NMU.xml onto it.
 
+netsh wlan delete profile name="NMU"
+
 $scriptPath = Get-Location
 
 $scriptPath = Join-Path  ($scriptPath) "PSScripts"
